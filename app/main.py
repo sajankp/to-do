e2d115a653c5ffd8edd31b96d17d7d1fd5b03d83
sajan <skpsajan@gmail.com>
2023-05-23 15:38:05 +0530

    Add database connection
    Create a database connection and store variables in .env

--- app/main.py
+++ app/main.py
@@ -1,15 +1,34 @@
-from typing import Union
-
+import os
+import urllib
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
+
+load_dotenv()
+
+password = urllib.parse.quote(os.getenv("MONGO_PASSWORD"), safe="")
+username = urllib.parse.quote(os.getenv("MONGO_USERNAME"), safe="")
+uri = f"mongodb+srv://{username}:{password}@cluster0.gbaxrnp.mongodb.net/?retryWrites=true&w=majority"
+
+
+def get_mongo_client():
+    client = MongoClient(uri)
+    return client
+
+
+@app.on_event("startup")
+async def startup():
+    app.mongodb_client = get_mongo_client()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.mongodb_client.close()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
