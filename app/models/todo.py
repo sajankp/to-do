218f98eeedadc36072936aea90a7ec29cf9087e6
sajankp <skpsajan@gmail.com>
2023-09-26 21:52:01 +0530

    Fix issues with the list route

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+from bson import ObjectId
 from pydantic import BaseModel, Field
 
 
@@ -28,8 +29,27 @@ class TodoCreate(TodoBase):
 class TodoUpdate(TodoBase):
     status: str = Field(...)
 
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 class Todo(TodoBase):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     created_at: datetime = Field(...)
     updated_at: datetime = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        # to handle the id field as a string
+        json_encoders = {ObjectId: str}
